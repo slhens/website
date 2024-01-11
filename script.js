7c77b4835f0e41417c800e807c031c88a991ed58
slhens <salihenescalik@gmail.com>
2024-01-12 01:12:56 +0300

    sadas

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-const checkbox = document.getElementById("checkbox")
-checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("dark")
-  
-})
+//karanlık mod
+body = document.querySelector("body"),
+toggle = document.querySelector(".toggle");
 
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark") {
+  body.classList.add("dark");
+  toggle.classList.add("active");
+}
+
+toggle.addEventListener("click", () => {
+  body.classList.toggle("dark");
+
+  if (!body.classList.contains("dark")) {
+    return localStorage.setItem("mode","light");
+  }
+  localStorage.setItem("mode","dark");
+});
+
+toggle.addEventListener("click", () => toggle.classList.toggle("active"));
+
+//saate göre ekranda yazan yazı
 var tarih = new Date();
 var saat = tarih.getHours();
 
 if(saat>=0 && saat<6) {
   document.getElementById("time").innerHTML = "İyi Geceler, Ben";
-  console.log(saat);
 }
 if(saat>=6 && saat<12) {
   document.getElementById("time").innerHTML = "Günaydın, Ben";
